7b06ab9a7c2b0b83db4228877d5f5a79d17bab11
DieBanane <Michael.Stiefel@gmail.com>
2018-05-24 09:22:36 +0200

    Updated utils.py and Instrument_tracking.py this should be the base for our common code

--- Instrument_Tracking.py
+++ Instrument_Tracking.py
@@ -1,71 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Author: Michael Stiefel
-Updated: 21.05.2018
+Author: Michael Stiefel and Prisca Dotti
+Updated: 24.05.2018
 
 This is the a script to do instrument tracking 
 """
 
 import numpy as np
-import cv2
 import matplotlib.pyplot as plt
-from skimage import io
-from skimage import filters
 import os
-import glob
 import time
-import matplotlib.pyplot as plt
 from tkinter import filedialog
 from tkinter import *
+from skimage import io
 
-def findPupilCenter (img):
-    #++++++++Detect Circle Center++++++++++++++
-    # Aplying heavy image blurring in order to vanish all disturbing features 
-    img = cv2.medianBlur(img,45)
-    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-    #cv2.imshow('Original Image',img)
-    
-    # Equalizing the histogramm in order to imporve the performance of the hough transform
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    equal = clahe.apply(img)
-    #cv2.imshow('Equalized Histogramm', equal)
-    #cv2.imshow('feature detection', cimg)
-    
-    # Applying Hough Circle Transform to identify the pupil center 
-    circle = cv2.HoughCircles(equal,cv2.HOUGH_GRADIENT,1,100,param1=50,param2=30,minRadius=75,maxRadius=150)
-    circle = np.uint16(np.around(circle))
-    for i in circle[0,:]:
-        # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-    #cv2.imshow('detected circles',cimg) 
-        
-    # Press 0 to close all windows (only needed for testing)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    
-    return cimg, circle 
-
-def findInstr(img, center_circle):
-    
-    x_center = center_circle[0][0][0]
-    y_center = center_circle[0][0][1]
-    radius = center_circle[0][0][2]
-    
-    mask = np.zeros(img.shape, dtype = 'uint8')
-    cv2.circle(mask,(x_center,y_center),radius,255,-1)
-    cv2.circle(mask,(x_center,y_center),np.uint16(np.ceil(0.75*radius)),0,-1)
-    img[mask == 0] = 0
-    
-    ret, thresh = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    
-    cv2.imshow('Mask',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    return 0
-
+# Import of all our functions defined in the utils.py script
+from utils import *
 
 
 # ++++++++++Search for images in selected folder+++++++++++++++
@@ -84,43 +34,45 @@ for root, dirs, files in os.walk(data.filepath):
              
 data.destroy()             
 
-imagelist = []
-circles_vector = []
 # +++++++++++++ Set save path ++++++++++
 data  = Tk()
 data.filepath = filedialog.askdirectory(initialdir = "/",title = "Select Directory to save results")
 savepath = data.filepath
 data.destroy()
 
-# +++++++++++++++ Main +++++++++++++++++++
-i = 0
-
+# +++++ This function imports all images that are in the selected folder
+# All files will be imported to the list called imagelist
+# The imported RGB-images are numpy arrays with format MxNx3
+imagelist = []
 for i in range(0,len(filelist)):
     if len(filelist) == len(pathlist):
         filepath = os.path.normpath(os.path.join(pathlist[i],filelist[i]))
     else:
         print('Filelist and Pathlist have not equal size')
     center = (0,0)
-    img = cv2.imread(filepath,0)
-    img_fill = cv2.medianBlur(img,51)
-    #sobel = cv2.Sobel(img_fill,cv2.CV_8U,1,0,ksize=9)
-#    laplacian = cv2.Laplacian(img_fill,cv2.CV_64F, ksize =17)
+    img = io.imread(filepath)
+    imagelist.append(img)
+
+# +++++++++++++++ Main +++++++++++++++++++
+# Some Global Variable that we need:
+first_center_x = 0  # x-Coordinate of the Instrument center in the first image
+first_center_y = 0  # y-Coordinate of the Instrument center in the first image
+
+
+    
+
+
+
+#Some chunck 
+#circles_vector = []
+##    cimg, circle = findPupilCenter(img)
+#    cimg,x_center, y_center = findPupilCenter2(img)
+##    if circle.shape == (1,1,3):
+##        circles_vector.append(circle)
+##    else:
+##        circles_vector.append([0,0,100])
 #    
-#    cv2.imshow('original image', img)
-#    cv2.imshow('filtered image', img_fill)
-#    cv2.imshow('laplacian_filtered', laplacian)
-#    cv2.waitKey(0)
-#    cv2.destroyAllWindows()
-    
-    cimg, circle = findPupilCenter(img)
-    imagelist.append(cimg)
-    if circle.shape == (1,1,3):
-        circles_vector.append(circle)
-    else:
-        circles_vector.append([0,0,100])
-    zero = findInstr(img,circles_vector[i])
-    
-    # ++++ Save annotated images +++++
-    save_img_path = os.path.normpath(os.path.join(savepath,filelist[i]))
-    print(save_img_path)
-    cv2.imwrite(save_img_path, cimg)
+#    # ++++ Save annotated images +++++
+#    save_img_path = os.path.normpath(os.path.join(savepath,filelist[i]))
+#    print(save_img_path)
+#    cv2.imwrite(save_img_path, cimg)
---
+++ utils.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 24 08:48:32 2018
+
+@author: Michael Stiefel and Prisca Dotti 
+
+**** This Script should contain all our functions used in the final version 
+of the Tracking allgorithm. Please try to keep it as clean as possible...:-)
+Please don't delete any function just add functions we will clean up at the end.
+"""
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from skimage import filters
+from skimage import exposure
+from skimage import data, color
+from skimage.draw import circle_perimeter
+from skimage.filters import threshold_otsu
+import scipy
+
+def findPupilCenter (img):
+    #++++++++Detect Circle Center++++++++++++++
+    # Aplying heavy image blurring in order to vanish all disturbing features 
+    img = cv2.medianBlur(img,45)
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    #cv2.imshow('Original Image',img)
+    
+    # Equalizing the histogramm in order to imporve the performance of the hough transform
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    equal = clahe.apply(img)
+    #cv2.imshow('Equalized Histogramm', equal)
+    #cv2.imshow('feature detection', cimg)
+    
+    # Applying Hough Circle Transform to identify the pupil center 
+    circle = cv2.HoughCircles(equal,cv2.HOUGH_GRADIENT,1,100,param1=50,param2=30,minRadius=75,maxRadius=150)
+    circle = np.uint16(np.around(circle))
+    for i in circle[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    #cv2.imshow('detected circles',cimg) 
+        
+    # Press 0 to close all windows (only needed for testing)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+    return cimg, circle
+    
+def findPupilCenter2 (img):
+    # Function to find circle center only with skimage functions
+    # Works only with grayscale images 
+
+    # Load picture
+    #image_raw = io.imread(filepath, as_grey= True)
+    image_raw = color.rgb2gray(image_color)
+    image =np.uint8(255*(image_raw/image_raw.max()))
+    plt.figure()
+    plt.imshow(image, cmap = plt.cm.gray)
+    image = filters.gaussian(image, sigma=31)
+    plt.figure()
+    plt.title('Gaussian Filtered Image')
+    plt.imshow(image, cmap = plt.cm.gray)
+    #image = exposure.equalize_hist(image)
+    low, high = np.percentile(image, (50, 90))
+    image = exposure.rescale_intensity(image, in_range=(low, high))
+    plt.figure()
+    plt.title('Rescaled intensity')
+    plt.imshow(image, cmap = plt.cm.gray)
+        
+    thresh = threshold_otsu(image, nbins=256)
+    image_threshold = image > thresh
+    plt.figure()
+    plt.title('Thresholded Image')
+    plt.imshow(image_threshold, cmap =plt.cm.gray)
+    
+    dist = scipy.ndimage.morphology.distance_transform_edt(image_threshold, return_distances=True, return_indices=False)
+    plt.figure()
+    plt.title('Distance Transform')
+    plt.imshow(dist, cmap =plt.cm.gray)
+    
+    indices = np.where(dist == dist.max())
+    
+    circy, circx = circle_perimeter(indices[0][0], indices[1][0], 10)
+    image_raw[circy, circx] = 0
+    plt.figure()
+    plt.title('Marked Image')
+    plt.imshow(image_raw, cmap =plt.cm.gray)
+    
+    return image_raw, indices[0][0], indices[1][0]
